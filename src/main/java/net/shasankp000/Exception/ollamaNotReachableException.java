e4e31e9288cc17c62976a1ec9e694c8106ca277b
shasankp000 <shasankp.14@gmail.com>
2024-07-22 23:36:48 +0530

    Created a new exception

---
+++ src/main/java/net/shasankp000/Exception/ollamaNotReachableException.java
@@ -0,0 +1,10 @@
+package net.shasankp000.Exception;
+
+public class ollamaNotReachableException extends Exception{
+
+    public ollamaNotReachableException(String message) {
+
+        super(message);
+    }
+
+}
