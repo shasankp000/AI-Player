c00144e61f3bc020cc5e52ad403186f38adb0f65
shasankp000 <shasankp.14@gmail.com>
2024-08-19 12:25:29 +0530

    Changed model dependency from llama2 to gemma2

--- src/main/java/net/shasankp000/ChatUtils/InputIntentConversationClassifier.java
+++ src/main/java/net/shasankp000/ChatUtils/InputIntentConversationClassifier.java
@@ -19,125 +19,64 @@ public class InputIntentConversationClassifier {
     private static final OllamaAPI ollamaAPI = new OllamaAPI(host);
 
     private static String buildPrompt() {
-        return "You are a first-principles reasoning intention classifier tool that takes a question/user prompt from a minecraft player and finds the intention of the question/prompt  Here are some example prompts that you may receive:\n" +
-                " 1. Hi there!\n" +
-                " 2. Hello!\n" +
-                " 3. So, how have you been?\n" +
-                " 4. What a marvelous day!\n" +
-                " 5. The sky looks beautiful?\n" +
-                " 6. Greetings!" +
-
-                "YOU ARE NOT TO INTRODUCE YOURSELF UNDER ANY CIRCUMSTANCE" +
-                "YOU ARE NOT TO INTERACT AS AN AI AGENT UNDER ANY CIRCUMSTANCE" +
-                "Note that you are not to reply directly or indirectly to these user prompts as these are NOT ADDRESSED TO YOU. YOU MUST NOT UNDER ANY CIRCUMSTANCE, REPLY TO THESE PROMPTS." +
-
-                "\n" +
-                "You need to find out whether the user prompt caters to this specific intention:\n" +
-                "\n" +
-                " 1. GENERAL_CONVERSATION: This intention corresponds towards just making general conversation or small talk with the minecraft bot.\n" +
-                "\n" +
-                " How to classify intentions:\n" +
-                "\n" +
-                " First of all, you need to know about the types of sentences in english grammar.\n" +
-                "\n" +
-                " Types of Sentences:\n" +
-                " Sentences can be classified into types based on two aspects – their function and their structure. They are categorised into four types based on their function and into three based on their structure. Assertive/declarative, interrogative, imperative and exclamatory sentences are the four types of sentences. The three types of sentences, according to the latter classification, are simple, complex and compound sentences.\n" +
-                "\n" +
-                " Let us look at each of these in detail.\n" +
-                "\n" +
-                " An assertive/declarative sentence is one that states a general fact, a habitual action, or a universal truth.  For example, ‘Today is Wednesday.’\n" +
-                " An imperative sentence is used to give a command or make a request. Unlike the other three types of sentences, imperative sentences do not always require a subject; they can start with a verb. For example, ‘Turn off the lights and fans when you leave the class.’\n" +
-                " An interrogative sentence asks a question. For example, ‘Where do you stay?’\n" +
-                " An exclamatory sentence expresses sudden emotions or feelings. For example, ‘What a wonderful sight!’\n" +
-                "\n" +
-                " Now, let us learn what simple, compound and complex sentences are. This categorisation is made based on the nature of clauses in the sentence.\n" +
-                "\n" +
-                " Simple sentences contain just one independent clause. For instance, ‘The dog chased the little wounded bird.’\n" +
-                " Compound sentences have two independent clauses joined together by a coordinating conjunction. For instance, ‘I like watching Marvel movies, but my friend likes watching DC movies.’\n" +
-                " Complex sentences have an independent clause and a dependent clause connected by a subordinating conjunction.  For example, ‘Though we were tired, we played another game of football.’\n" +
-                " Complex-compound sentences have two independent clauses and a dependent clause. For instance, ‘Although we knew it would rain, we did not carry an umbrella, so we got wet.’\n" +
-                "\n" +
-                "\n" +
-                " Now based on these types you can detect the intention of the sentence.\n" +
-                "\n" +
-                " For example: Most sentences beginning with the words: \"Please, Could, Can, Will, Will you\" have the intention of requesting something and thus in the context of minecraft will invoke the REQUEST_ACTION intention.\n" +
-                " For sentences beginning with : \"What, why, who, where, when, Did, Did you\" have the intention of asking something. These are of type interrogative sentences and will invoke the ASK_INFORMATION intention within the context of minecraft.\n" +
-                " For sentences simply beginning with action verbs like : \"Go, Do, Craft, Build, Hunt, Attack\" are generally of type of imperative sentences as these are directly commanding you to do something. Such sentences will invoke the REQUEST_ACTION intention within the context of minecraft.\n" +
-                "\n" +
-                "And for normal sentences like : \"I ate a sandwich today\" or \"The weather is nice today\", these are declarative/assertive sentences, and within the context of minecraft, will invoke the intention of GENERAL_CONVERSATION.\n" +
-                "\n" +
-                "Anything outside of this lacks context and will invoke the intention of UNSPECIFIED within the context of minecraft.\n" +
-                "\n" +
-                "A few more examples for your better learning.\n" +
-                "\n" +
-                "Examples:\n" +
-                "\n" +
-                "\n" +
-                "INTENTION: GENERAL_CONVERSATION:\n" +
-                "I built a house today.\n" +
-                "The sky looks really clear.\n" +
-                "I love exploring caves.\n" +
-                "My friend joined the game earlier.\n" +
-                "This is a fun server.\n" +
-                "So, what was your name again?" +
-
-                "Any assertive/declarative sentence invokes GENERAL_CONVERSATION" +
-
-                "INTENTION: UNSPECIFIED: \n" +
-                "?\n" +
-                "Huh?\n" +
-                "The cave....\n" +
-                "Bananas\n" +
-                "What?\n" +
-                "....." +
-
-                "Basically anything which doesn't make sense or doesn't provide enough context falls under UNSPECIFIED" +
-                "When you receive such prompts with inadequate context, ONLY RESPOND AS UNSPECIFIED. DO NOT RESPOND WITH ANYTHING ELSE OR AS ANYTHING ELSE UNDER ANY CIRCUMSTANCES."+
-
-                "INTENTION: ASK_INFORMATION: \n" +
-                "Did you find any diamonds?\n" +
-                "Where are the closest villagers?\n" +
-                "What time is it in the game?\n" +
-                "How many hearts do you have left?\n" +
-                "Why is the sun setting so quickly?\n" +
-                "How do I craft a shield?"+
-                "Tell me the recipe of a fire resistance potion"+
-                "How do I craft wooden planks?"+
-
-                "Basically any prompt that starts with interrogative words like what, who, when, where, how, what if, etc all will invoke ASK_INFORMATION" +
-                "Any interrogative sentence invokes ASK_INFORMATION" +
-
-                "INTENTION: REQUEST_ACTION: \n"+
-                "Could you mine some stone and bring them to me?\n" +
-                "Please craft a set of iron armor.\n" +
-                "Go to coordinates 10 -60 11.\n" +
-                "Attack the nearest hostile mob.\n" +
-                "Build a shelter before nightfall.\n" +
-
-                "Any imperative sentence invokes REQUEST_ACTION" +
-
-
-                "\n" +
-                "However, sometimes you may encounter prompts which at first glance may look like general conversation, but they are actually not so:\n" +
-                "\n" +
-                "Sometimes sentences like \"So, did you go somewhere recently?\" or \"So Steve, How do I craft a shield?\" means to ASK_INFORMATION (which is another intention) while making conversation. Thus, remember to analyze the entire sentence.\n" +
-
-                "Sometimes sentences like \"So, how do I craft a shield?\",  or  \"Tell me the recipe of a fire resistance potion\" may appear to be requesting an action, but in reality it's about asking information regarding these topics, about crafting a shield or making a fire resistance potion in these examples."+
-                "Thus, before arriving to a final conclusion, please analyze the sentences properly."+
-
-                "\n" +
-                "RESPOND ONLY AS THE AFOREMENTIONED INTENTION TAGS, i.e GENERAL_CONVERSATION, REQUEST_ACTION, ASK_INFORMATION OR UNSPECIFIED, NOT A SINGLE WORD MORE.\n" +
-                "\n" +
-                "\n" +
-                " While returning the intention output, do not say anything else. By anything else, I mean any other word at all. \n" +
-                "Do not worry about actually executing this corresponding methods based on the user prompts or conversing with the user, that will be taken care of by another system by analyzing your output. \n" +
-                "Thus it is imperative that you output only the intention, and nothing else. \n";
+        return """
+                You are a first-principles reasoning intention classifier tool that takes a question/user prompt from a Minecraft player and finds the intention of the question/prompt. Here are some example prompts that you may receive:
+                
+                1. Hi there!
+                2. Hello!
+                3. So, how have you been?
+                4. What a marvelous day!
+                5. The sky looks beautiful?
+                6. Greetings!
+                
+                YOUR TASK is to classify the user's intention based on their prompt. You will not respond directly to the user prompt, only classify the intention. You must respond with only one of the following tags:
+                
+                - GENERAL_CONVERSATION: For casual greetings, remarks, or any small talk that does not involve a request or query.
+                - ASK_INFORMATION: For prompts that ask for information, typically starting with words like 'what,' 'how,' 'where,' 'when,' etc.
+                - REQUEST_ACTION: For commands or requests asking the bot to perform an action, usually starting with verbs like 'go,' 'do,' 'build,' 'craft,' etc.
+                - UNSPECIFIED: For prompts that lack sufficient context or are nonsensical.
+                
+                Examples:
+                
+                **GENERAL_CONVERSATION:**
+                - Hi Steve!
+                - The weather is nice today.
+                - I built a house earlier.
+                
+                **ASK_INFORMATION:**
+                - Did you find any diamonds?
+                - How do I craft a shield?
+                - Where are the closest villagers?
+                
+                **REQUEST_ACTION:**
+                - Go to coordinates 10 -60 11.
+                - Please craft a set of iron armor.
+                - Build a shelter before nightfall.
+                
+                **UNSPECIFIED:**
+                - ?
+                - Huh?
+                - Bananas
+                
+                However do note that sometimes what may appear to be as something, is usually not what you expect it to be.
+                
+                For example, sentences starting with can, could, please and other verbs and ending with a question-mark ? may appear to be of the intention ASK_INFORMATION.
+                
+                However you must analyze the entire sentence.
+                
+                For example on the context of block detection, the user may say "Please detect if there is a block in front of you." This may look like ASK_INFORMATION, but in reality it is of type REQUEST_ACTION.
+                
+                The fact that the intention's name is REQUEST_ACTION means that you can expect words like Please, Can, Could, Would, anything which prompts a question, even ends with a question mark, to be sometimes of type REQUEST_ACTION.
+                
+                So you must analyze the entire sentence very carefully before answering.
+                
+                ONLY respond with the intention tags (GENERAL_CONVERSATION, ASK_INFORMATION, REQUEST_ACTION, UNSPECIFIED).""";
 
     }
 
     public static InputIntentConversationClassifier.Intent getConversationIntent(String userPrompt) {
 
-        OllamaChatRequestBuilder builder = OllamaChatRequestBuilder.getInstance(OllamaModelType.MISTRAL); // LLAMA2 is surprisingly much less error-prone compared to phi3.
+        OllamaChatRequestBuilder builder = OllamaChatRequestBuilder.getInstance(OllamaModelType.GEMMA2);
 
         String systemPrompt = buildPrompt();
         String response;
